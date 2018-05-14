e3327256c959c40f39984871a22086b6256f9ce8
usercont <jpopelka@redhat.com>
2018-05-14 17:50:14 +0200

    0.0.15 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='release-bot-test',
-    version='0.0.14',
+    version='0.0.15',
     packages=['release_bot_test'],
     url='https://github.com/jpopelka/release-bot-test',
     license='GPLv3+',
