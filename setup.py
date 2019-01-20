f49a1375d77d35b161e47c8c376cdfce88ebe1f0
Jiri Popelka <jpopelka@redhat.com>
2019-01-20 18:45:23 +0100

    bump version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='release-bot-test',
-    version='0.0.22',
+    version='0.0.23',
     packages=['release_bot_test'],
     url='https://github.com/jpopelka/release-bot-test',
     license='GPLv3+',
